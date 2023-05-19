7ea9e46830fc6bf9d80131b88144b889278a1689
Brian <heynairb@gmail.com>
2023-05-19 13:56:31 -0700

    got all of state working except http request

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ pub mod task {
         }
     }
 
-    #[derive(Clone, Copy, Debug)]
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
     pub enum TaskState {
         NotStarted,
         Started,
@@ -88,7 +88,7 @@ pub mod task {
         }
     }
 
-    #[derive(Debug, Clone, Copy)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     pub enum TaskType {
         Foo(Option<TaskId>, TaskTimeStamp, TaskState),
         Bar(Option<TaskId>, TaskTimeStamp, TaskState),
@@ -123,9 +123,11 @@ pub mod task {
                     self.transition_task()
                 }
                 Self::Bar(_, _, TaskState::Started) => {
-                    let url = "https://www.whattimeisitrightnow.com/";
-                    if let Ok(response) = reqwest::get(url).await {
+                    let url = "https://www.whattimeisitrightnow.com";
+                    if let Ok(response) = reqwest::(url).await {
                         let status_code = response.status();
+                        let text = response.text().await.unwrap();
+                        println!("{text}");
                         let status_code_str = status_code.as_str();
                         std::println!("{status_code_str}");
                         self.transition_task()
@@ -147,6 +149,7 @@ mod state {
     use crate::task::TaskType::{Bar, Baz, Foo};
     use deadpool_sqlite::{Config, Pool, Runtime};
     use std::time::{SystemTime, UNIX_EPOCH};
+    use rusqlite::OptionalExtension;
     use tokio::task::JoinHandle;
     use crate::task;
 
@@ -154,11 +157,11 @@ mod state {
     pub trait State: Sized {
         async fn initialize_state() -> Self;
         async fn get_tasks_that_need_to_be_executed(&self) -> Vec<TaskType>;
-        async fn add_task(&self, task: TaskType);
+        async fn add_task(&self, task: TaskType) -> u64;
         async fn get_all_tasks(&self, task_state: &TaskState) -> Vec<TaskType>;
-
         async fn consume_tasks(&self, tasks_not_started: Vec<TaskType>) -> Vec<JoinHandle<TaskType>>;
-
+        async fn delete_task_from_id(&self, id: u64);
+        async fn get_task_from_id(&self, id: u64) -> Option<TaskType>;
         async fn clear_all(&self);
     }
 
@@ -193,9 +196,7 @@ mod state {
             }
         }
 
-        async fn consume_tasks(&self, tasks_not_started: Vec<TaskType>) -> Vec<JoinHandle<TaskType>>{
-
-
+        async fn consume_tasks(&self, tasks_not_started: Vec<TaskType>) -> Vec<JoinHandle<TaskType>> {
             let mut join_handles = Vec::with_capacity(tasks_not_started.len());
             let mut_ref_join_handler = &mut join_handles;
             for task in tasks_not_started.into_iter() {
@@ -259,23 +260,33 @@ mod state {
             }).await.unwrap()
         }
 
-        async fn add_task(&self, task: TaskType) {
+        async fn add_task(&self, task: TaskType) -> u64 {
             let (task_type, optional_id, time, state): (&str, Option<u64>, u64, &str) = match task {
                 TaskType::Foo(id, time, state) => (TaskType::FOO, id, time, (&state).into()),
                 TaskType::Bar(id, time, state) => (TaskType::BAR, id, time, (&state).into()),
                 TaskType::Baz(id, time, state) => (TaskType::BAZ, id, time, (&state).into())
             };
             let connection = self.pool.get().await.unwrap();
-            let rows_updated = connection.interact(move |connection| {
-                if let Some(id) = optional_id {
-                    connection.execute("INSERT INTO tasks (id, task_time, task_type, task_state) VALUES (?1, ?2, ?3, ?4)", (id, time, task_type, state)).unwrap()
-                } else {
-                    connection.execute("INSERT INTO tasks (task_time, task_type, task_state) VALUES (?1, ?2, ?3)", (time, task_type, state)).unwrap()
+            connection.interact(move |connection| {
+                let mut rows_updated: usize;
+                let id = match optional_id {
+                    Some(id) => {
+                        rows_updated = connection.execute("INSERT INTO tasks (id, task_time, task_type, task_state) VALUES (?1, ?2, ?3, ?4)", (id, time, task_type, state)).unwrap();
+                        id
+                    }
+                    None => {
+                        let transaction = connection.transaction().unwrap();
+                        rows_updated = transaction.execute("INSERT INTO tasks (task_time, task_type, task_state) VALUES (?1, ?2, ?3)", (time, task_type, state)).unwrap();
+                        let id: u64 = transaction.last_insert_rowid() as u64;
+                        transaction.commit().unwrap();
+                        id
+                    }
+                };
+                if rows_updated != 1 {
+                    panic!("insert says less or more than 1 row was updated. This is a error with the code, only one row should be inserted")
                 }
-            }).await.unwrap();
-            if rows_updated != 1 {
-                panic!("insert says less or more than 1 row was updated.")
-            }
+                id
+            }).await.unwrap()
         }
 
         async fn get_all_tasks(&self, task_state: &TaskState) -> Vec<TaskType> {
@@ -307,6 +318,30 @@ mod state {
             connection.interact(|connection| {
                 connection.execute("DELETE FROM tasks;", ()).unwrap()
             }).await.unwrap();
+        }
+
+        async fn delete_task_from_id(&self, id: u64) {
+            let connection = self.pool.get().await.unwrap();
+            connection.interact(move |connection| {
+                connection.execute("DELETE FROM tasks WHERE id = ?1;", (id, )).unwrap()
+            }).await.unwrap();
+        }
+
+        async fn get_task_from_id(&self, id: u64) -> Option<TaskType> {
+            let connection = self.pool.get().await.unwrap();
+            connection.interact(move |connection| {
+                connection.query_row("SELECT id, task_time, task_type, task_state FROM tasks WHERE tasks.id = ?1 LIMIT 1", (id, ), |row| {
+                    let (id, time, task_type, state): (u64, u64, String, String) = (row.get(0).unwrap(), row.get(1).unwrap(), row.get(2).unwrap(), row.get(3).unwrap());
+                    Ok(match task_type.as_str() {
+                        TaskType::FOO => Foo(Some(id), time, state.as_str().into()),
+                        TaskType::BAR => Bar(Some(id), time, state.as_str().into()),
+                        TaskType::BAZ => Baz(Some(id), time, state.as_str().into()),
+                        _ => {
+                            panic!("invalid string used for task_type.")
+                        }
+                    })
+                }).optional().unwrap()
+            }).await.unwrap()
         }
     }
 }
@@ -341,7 +376,7 @@ mod tests {
     use crate::state;
     use crate::state::State;
     use crate::task::TaskState::{Complete, NotStarted, Started};
-    use crate::task::TaskType::{Baz, Foo};
+    use crate::task::TaskType::{Bar, Baz, Foo};
     use std::time::{SystemTime, UNIX_EPOCH};
     use async_std::prelude::FutureExt;
 
@@ -349,15 +384,20 @@ mod tests {
     async fn test_state() {
         let state = state::SqliteState::initialize_state().await;
         state.clear_all().await;
-        let task = Foo(None, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 1000000, NotStarted);
-        state.add_task(task.clone()).await;
-        state.add_task(task.clone()).await;
+        let future_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 1000000;
+        let task = Foo(None, future_time, NotStarted);
+        let id1 = state.add_task(task.clone()).await;
+        assert_eq!(id1, 1);
+        let id2 = state.add_task(task.clone()).await;
+        assert_eq!(id2, 2);
         let v = state.get_all_tasks(&NotStarted).await;
         assert_eq!(v.len(), 2);
         let v2 = state.get_tasks_that_need_to_be_executed().await;
         assert_eq!(v2.len(), 0);
-        let future_task = Baz(None, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 2, NotStarted);
-        state.add_task(future_task).await;
+        let near_future_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 2;
+        let future_task = Baz(None, near_future_time, NotStarted);
+        let id3 = state.add_task(future_task.clone()).await;
+        assert_eq!(id3, 3);
         let v3 = state.get_tasks_that_need_to_be_executed().await;
         assert_eq!(v3.len(), 0);
         tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
@@ -373,5 +413,32 @@ mod tests {
         assert_eq!(v6.len(), 0);
         let v7 = state.get_all_tasks(&NotStarted).await;
         assert_eq!(v7.len(), 2);
+        let task1 = state.get_task_from_id(1).await.unwrap();
+        let task2 = state.get_task_from_id(2).await.unwrap();
+        let task3 = state.get_task_from_id(3).await.unwrap();
+        assert_eq!(task1, Foo(Some(1), future_time, NotStarted));
+        assert_eq!(task2, Foo(Some(2), future_time, NotStarted));
+        assert_eq!(task3, Baz(Some(3), near_future_time, Complete));
+        for id in 1..4 {
+            state.delete_task_from_id(id).await;
+        }
+        for id in 1..4 {
+            let val = state.get_task_from_id(id).await;
+            assert_eq!(val, None)
+        }
+        let near_future_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + 2;
+        let test_foo = Foo(None, 0, NotStarted);
+        let test_bar = Bar(None, 0, NotStarted);
+        state.add_task(test_foo).await;
+        state.add_task(test_bar).await;
+        let tasks_to_consume = state.get_all_tasks(&NotStarted).await;
+        assert_eq!(tasks_to_consume.len(), 2);
+        let handlers2 = state.consume_tasks(tasks_to_consume).await;
+        for h in handlers2 {
+            h.await.unwrap();
+        }
+        assert_eq!(state.get_all_tasks(&NotStarted).await.len(), 0);
+        assert_eq!(state.get_all_tasks(&Complete).await.len(), 2);
+
     }
 }
